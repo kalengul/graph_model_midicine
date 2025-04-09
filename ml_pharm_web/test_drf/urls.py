b976c70bdde89428c03f61e1237639a3892b9fc3
Philipekum <filipp.tsekhansky@yandex.ru>
2025-04-09 14:11:05 +0300

    added test menu API-call

--- ml_pharm_web/test_drf/urls.py
+++ ml_pharm_web/test_drf/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HelloWorld
+from .views import HelloWorld, Menu
 
 
 urlpatterns = [
     path('hello/', HelloWorld.as_view(), name='hello_world'),
+    path('menu/', Menu.as_view(), name='menu')
 ]
--- ml_pharm_web/test_drf/views.py
+++ ml_pharm_web/test_drf/views.py
@@ -5,4 +5,30 @@ from rest_framework.response import Response
 class HelloWorld(APIView):
     def get(self, request):
         return Response({"message": "Hello, Polina!"})
+
+
+class Menu(APIView):
+    def get(self, _):
+        res = {
+            "result": {
+                "status": 200,
+                "message": "меню получено успешно"
+            },
+            "data": [
+                {
+                    "title": "Взаимодействие MedScape",
+                    "slug": "/medScape"
+                },
+                {
+                    "title": "Взаимодействие Fortran",
+                    "slug": "/fortran"
+                },
+                {
+                    "title": "Добавить ЛС",
+                    "slug": "/addDrug"
+                }
+            ]
+        }
+
+        return Response(res)
     
