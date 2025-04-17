138a4ad75c454f835c6d126e4f2f02f778ee0b04
KubrakovDmitry <kubrakoff.dmitry@yandex.ru>
2025-04-17 19:08:34 +0300

    Adding of deleting drug, side effect, drug group

--- ml_pharm_web/drugs/views.py
+++ ml_pharm_web/drugs/views.py
@@ -105,12 +105,12 @@ class DrugGroupAPI(APIView):
             instance.delete()
             return CustomResponse.response(
                 status=status.HTTP_200_OK,
-                message=f'Групп "{instance.dg_name}" удалена',
+                message=f'Группа {instance.dg_name} удалена',
                 http_status=status.HTTP_200_OK)
         except DrugGroup.DoesNotExist:
             return CustomResponse.response(
                 status=status.HTTP_400_BAD_REQUEST,
-                message='Ошибка определения удаляемой группы ЛС',
+                message='Ошибка определения удаляемого объекта',
                 http_status=status.HTTP_400_BAD_REQUEST)
         except Exception:
             return CustomResponse.response(
@@ -202,7 +202,7 @@ class DrugAPI(APIView):
                 message=SERVER_ERROR,
                 http_status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def detele(self, request):
+    def delete(self, request):
         """Метод для DELETE-запросов."""
         try:
             instance = Drug.objects.get(
@@ -210,7 +210,7 @@ class DrugAPI(APIView):
             instance.delete()
             return CustomResponse.response(
                 status=status.HTTP_200_OK,
-                message=f'ЛС "{instance.drug_name}" удалено',
+                message=f'Лекарственное средство {instance.drug_name} удалено',
                 http_status=status.HTTP_200_OK)
         except Drug.DoesNotExist:
             return CustomResponse.response(
@@ -222,144 +222,6 @@ class DrugAPI(APIView):
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 message=SERVER_ERROR,
                 http_status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-# class AddDrugGroupAPI(APIView):
-#     """Вью-класс для создания групп ЛС."""
-
-#     def post(self, request):
-#         """Метод для запросов POST."""
-#         serializer = DrugGroupSerializer(data=request.data)
-#         if serializer.is_valid():
-#             try:
-#                 serializer.save()
-#                 return CustomResponse.response(
-#                     status=status.HTTP_200_OK,
-#                     message=f'Группа ЛС {request.data.get('dg_name')}',
-#                     http_status=status.HTTP_200_OK)
-#             except IntegrityError:
-#                 return CustomResponse.response(
-#                     status=status.HTTP_400_BAD_REQUEST,
-#                     message=f'Группа {request.data.get('dg_name')} уже существует',
-#                     http_status=status.HTTP_400_BAD_REQUEST
-#                 )
-#             except Exception:
-#                 return CustomResponse.response(
-#                     status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-#                     message=SERVER_ERROR,
-#                     http_status=status.HTTP_500_INTERNAL_SERVER_ERROR
-#                     )
-#         return CustomResponse.response(
-#             status=status.HTTP_400_BAD_REQUEST,
-#             message=INCORRECT_DATA,
-#             http_status=status.HTTP_400_BAD_REQUEST
-#         )
-
-
-# class GetDrugGroupAPI(APIView):
-#     """Вью-класс для получения групп ЛС."""
-
-#     def get(self, request):
-#         """Пример вью, которая возвращает одну группу по id."""
-#         pk = request.query_params.get('id')
-#         if not pk:
-#             return Response(
-#                 {"error": "Необходимо указать ID группы"},
-#                 status=status.HTTP_400_BAD_REQUEST
-#             )
-#         try:
-#             group = DrugGroup.objects.get(pk=pk)
-#             serializer = DrugGroupSerializer(group)
-#             return Response(serializer.data)
-#         except ObjectDoesNotExist:
-#             return Response(
-#                 {"error": "Группа ЛС не найдена"},
-#                 status=status.HTTP_404_NOT_FOUND
-#             )
-
-
-# class AddDrugAPI(APIView):
-#     """Вью-класс для создания ЛС."""
-
-#     def post(self, request):
-#         """Метод для запросов POST."""
-#         serializer = DrugSerializer(data=request.data)
-#         if serializer.is_valid():
-#             try:
-#                 serializer.save()
-#                 return CustomResponse.response(
-#                     status=status.HTTP_200_OK,
-#                     message=f"ЛС {request.data.get('drug_name')} добавлен",
-#                     http_status=status.HTTP_200_OK)
-#             except IntegrityError as e:
-#                 if 'slug' in str(e):
-#                     return CustomResponse.response(
-#                         status=status.HTTP_400_BAD_REQUEST,
-#                         message=f"ЛС {request.data.get('drug_name')} уже существует",
-#                         http_status=status.HTTP_400_BAD_REQUEST
-#                     )
-#                 return CustomResponse.response(
-#                     status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-#                     message='Ошибка при создании лекарства',
-#                     http_status=status.HTTP_500_INTERNAL_SERVER_ERROR
-#                 )
-#         return CustomResponse.response(
-#             status=status.HTTP_400_BAD_REQUEST,
-#             message=INCORRECT_DATA,
-#             http_status=status.HTTP_400_BAD_REQUEST
-#         )
-
-
-# class GetDrugAPI(APIView):
-#     """
-#     GET api/v1/getDrug/?drug_id={id}.
-
-#     Если drug_id не указан — вернуть список всех ЛС.
-#     Если указан — вернуть одно ЛС.
-#     """
-
-#     def get(self, request):
-#         """Метод для запросов GET."""
-#         drug_id = request.query_params.get('drug_id')
-
-#         # Если drug_id не указан, возвращаем список всех ЛС
-#         if not drug_id:
-#             drugs = Drug.objects.all()
-#             serializer = DrugListRetrieveSerializer(drugs, many=True)
-#             return Response({
-#                 "result": {
-#                     "status": 200,
-#                     "message": "Список ЛС получен"
-#                 },
-#                 "data": serializer.data
-#             }, status=status.HTTP_200_OK)
-
-#         # Если drug_id указан, пытаемся получить одно ЛС
-#         try:
-#             drug = Drug.objects.get(pk=drug_id)
-#             serializer = DrugListRetrieveSerializer(drug)
-#             return Response({
-#                 "result": {
-#                     "status": 200,
-#                     "message": "ЛС получено"
-#                 },
-#                 "data": serializer.data
-#             }, status=status.HTTP_200_OK)
-#         except Drug.DoesNotExist:
-#             return Response({
-#                 "result": {
-#                     "status": 404,
-#                     "message": "Лекарственное средство не найдено"
-#                 },
-#                 "data": {}
-#             }, status=status.HTTP_404_NOT_FOUND)
-#         except Exception:
-#             return Response({
-#                 "result": {
-#                     "status": 500,
-#                     "message": SERVER_ERROR
-#                 }
-#             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class SideEffectAPI(APIView):
@@ -460,66 +322,13 @@ class SideEffectAPI(APIView):
         except SideEffect.DoesNotExist:
             return CustomResponse.response(
                 status=status.HTTP_400_BAD_REQUEST,
-                message='Ошибка определения удаляемого побочного действия',
+                message='Ошибка определения удаляемого объекта',
                 http_status=status.HTTP_400_BAD_REQUEST)
         except Exception:
             return CustomResponse.response(
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 message=SERVER_ERROR,
                 http_status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-# class GetSideEffectAPI(APIView):
-#     """
-#     GET api/v1/getSideEffect/?se_id={id}.
-
-#     Если se_id не указан — вернуть список всех побочных эффектов.
-#     Если указан — вернуть один.
-#     """
-
-#     def get(self, request):
-#         """Метод для запросов GET."""
-#         se_id = request.query_params.get('se_id')
-
-#         # Если se_id не указан, возвращаем список всех
-#         if not se_id:
-#             side_effects = SideEffect.objects.all()
-#             serializer = SideEffectListRetrieveSerializer(side_effects,
-#                                                           many=True)
-#             return Response({
-#                 "result": {
-#                     "status": 200,
-#                     "message": "Список побочных эффектов получен"
-#                 },
-#                 "data": serializer.data
-#             }, status=status.HTTP_200_OK)
-
-#         # Если se_id указан, пытаемся получить один
-#         try:
-#             se = SideEffect.objects.get(pk=se_id)
-#             serializer = SideEffectListRetrieveSerializer(se)
-#             return Response({
-#                 "result": {
-#                     "status": 200,
-#                     "message": "Побочный эффект получен"
-#                 },
-#                 "data": serializer.data
-#             }, status=status.HTTP_200_OK)
-#         except SideEffect.DoesNotExist:
-#             return Response({
-#                 "result": {
-#                     "status": 404,
-#                     "message": "Побочный эффект не найден"
-#                 },
-#                 "data": {}
-#             }, status=status.HTTP_404_NOT_FOUND)
-#         except Exception:
-#             return Response({
-#                 "result": {
-#                     "status": 500,
-#                     "message": SERVER_ERROR
-#                 }
-#             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class DrugSideEffectView(APIView):
@@ -575,7 +384,8 @@ class DrugSideEffectView(APIView):
             except DrugSideEffect.DoesNotExist:
                 return CustomResponse.response(
                     status=status.HTTP_404_NOT_FOUND,
-                    message=f'Связь drug_id={drug_id} и se_id={se_id} не найдена',
+                    message=(f'Связь drug_id={drug_id} '
+                             f'и se_id={se_id} не найдена'),
                     http_status=status.HTTP_404_NOT_FOUND
                 )
 
@@ -615,61 +425,6 @@ class DrugSideEffectView(APIView):
                     status=status.HTTP_500_INTERNAL_SERVER_ERROR,
                     message="Неизвестная ошибка сервера",
                     http_status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-# class DrugSideEffectView(APIView):
-#     """Вью для работы с рангами."""
-
-#     def put(self, request):
-#         """Метод для запроса PUT."""
-#         drug_id = request.data.get('drug_id')
-#         if not drug_id:
-#             return CustomResponse.response(
-#                 status=status.HTTP_400_BAD_REQUEST,
-#                 message='id ЛС не передан',
-#                 http_status=status.HTTP_400_BAD_REQUEST)
-#         se_id = request.data.get('se_id')
-
-#         if not se_id:
-#             return CustomResponse.response(
-#                 status=status.HTTP_400_BAD_REQUEST,
-#                 message='id побочного действия не передан')
-
-#         if not Drug.objects.filter(id=drug_id).exists():
-#             return CustomResponse.response(
-#                 status=status.HTTP_404_NOT_FOUND,
-#                 message='Такого ЛС не существует',
-#                 http_status=status.HTTP_404_NOT_FOUND)
-#         if not SideEffect.objects.filter(id=se_id).exists():
-#             return CustomResponse.response(
-#                 status=status.HTTP_404_NOT_FOUND,
-#                 message='Такого побочного эффекта не существует',
-#                 http_status=status.HTTP_404_NOT_FOUND)
-
-#         try:
-#             drug_side_effect = DrugSideEffect.objects.get(
-#                 drug_id=drug_id,
-#                 side_effect_id=se_id)
-#         except DrugSideEffect.DoesNotExist:
-#             return CustomResponse.response(
-#                 status=status.HTTP_404_NOT_FOUND,
-#                 message='Нет такого ранга',
-#                 http_status=status.HTTP_404_NOT_FOUND
-#             )
-#         serializer = DrugSideEffectSerializer(drug_side_effect,
-#                                               data=request.data)
-
-#         if serializer.is_valid():
-#             serializer.save()
-#             return CustomResponse.response(
-#                 status=status.HTTP_200_OK,
-#                 message='Ранги обновлены',
-#                 http_status=status.HTTP_200_OK
-#             )
-#         return CustomResponse.response(
-#                 status=status.HTTP_404_NOT_FOUND,
-#                 message='Ранг задан некорректно',
-#                 http_status=status.HTTP_404_NOT_FOUND
-#             )
 
 
 class MultiDeleteView(APIView):
