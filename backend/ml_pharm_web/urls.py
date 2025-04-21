a735a7c1eb0200ddbb1cbee84f2b34450ef9ea68
Philipekum <filipp.tsekhansky@yandex.ru>
2025-04-21 18:13:13 +0300

    import fix

--- backend/ml_pharm_web/urls.py
+++ backend/ml_pharm_web/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from backend import settings
+from . import settings
 
 
 urlpatterns = [
