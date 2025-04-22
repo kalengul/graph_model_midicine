55a261d8b4b746179b9966be31f00e92534df343
Polina <polianna_654@mail.ru>
2025-04-22 13:04:26 +0300

    Исправлен build

--- frontend/src/components/sideEffectsTable/sideEffectsTable.tsx
+++ frontend/src/components/sideEffectsTable/sideEffectsTable.tsx
@@ -3,7 +3,7 @@ import {updateRank} from "../../redux/SideEffectManageSlice"
 import "./sideEffectsTable.scss"
 
 import { debounce } from 'lodash';
-import {useMemo} from "react"
+import {useMemo, useCallback} from "react"
 
 export const SideEffectsTable = ()=>{
     const dispatch = useAppDispatch()
@@ -12,10 +12,17 @@ export const SideEffectsTable = ()=>{
     const sideEffects = useAppSelector((state)=>state.sideEffectManage.sideEffects)
     const ranks = useAppSelector((state)=>state.sideEffectManage.ranks)
 
-    const GetRankHandler = (drug_id: string, se_id: string) =>{
-        const rank = ranks.find(rank=>rank.drug_id===drug_id && rank.se_id ===se_id)
-        return  rank ? rank.rank : 0
-    }
+    const ranksMap = useMemo(() => {
+        const map = new Map<string, string>();
+        ranks.forEach(rank => {
+          map.set(`${rank.drug_id}-${rank.se_id}`, rank.rank);
+        });
+        return map;
+      }, [ranks]);
+
+    const GetRankHandler = useCallback((drug_id: string, se_id: string) => {
+        return ranksMap.get(`${drug_id}-${se_id}`) || '0';
+    }, [ranksMap]);
 
     const rankChangeHandler =  useMemo(() => debounce(
         (drug_id: string, se_id: string, newRank: string) => {
@@ -23,6 +30,7 @@ export const SideEffectsTable = ()=>{
         },
         300 // Задержка 300мс
     ), [dispatch])
+    
 
     return (
     <div className='SE-table scroll'>
@@ -42,7 +50,7 @@ export const SideEffectsTable = ()=>{
                         <th className="sticky-col-1">{index + 1}</th>
                         <th className="sticky-col-2">{sideEffect.se_name}</th>
                         {Array.isArray(drugs) && drugs.map((drug) =>
-                            <td key={`${drug.id} ${sideEffect.id}`} >
+                            <td key={`${drug.id}-${sideEffect.id}`} >
                                 <input
                                     type="text"
                                     value={GetRankHandler(drug.id, sideEffect.id)}
