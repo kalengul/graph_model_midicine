e3cc669133dd08f7f5e3c31dcb30f5b141a6134f
Philipekum <filipp.tsekhansky@yandex.ru>
2025-04-23 14:45:47 +0300

    customresponse for accounts

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,28 +1,39 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status, permissions
-from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate
+from drugs.utils.custom_response import CustomResponse
 
 
 class LoginUser(APIView):
-    permission_classes = [AllowAny]
+    permission_classes = [permissions.AllowAny]
 
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
 
         if not username or not password:
-            return Response({'error': 'Username and password are required'}, status=status.HTTP_400_BAD_REQUEST)
+            return CustomResponse.response(
+                status=400,
+                message="Логин и пароль обязательны",
+                http_status=status.HTTP_400_BAD_REQUEST
+            )
 
         user = authenticate(username=username, password=password)
-        if user is not None:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key})
-        
-        else:
-            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        if user is None:
+            return CustomResponse.response(
+                status=401,
+                message="Неверные учетные данные",
+                http_status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        token, created = Token.objects.get_or_create(user=user)
+        return CustomResponse.response(
+            data={"token": token.key},
+            status=200,
+            message="Авторизация прошла успешно",
+            http_status=status.HTTP_200_OK
+        )
 
 
 class LogoutUser(APIView):
@@ -32,6 +43,9 @@ class LogoutUser(APIView):
         try:
             request.user.auth_token.delete()
         except Token.DoesNotExist:
-            pass 
-        return Response({"detail": "Logged out"}, status=status.HTTP_200_OK)
-    
+            pass  
+
+        return CustomResponse.response(
+            message="Выход выполнен успешно",
+            http_status=status.HTTP_200_OK
+        )
