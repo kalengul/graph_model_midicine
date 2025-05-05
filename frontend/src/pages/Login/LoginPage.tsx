213c35724631d193265f595282bff36cf91abcb8
Polina <polianna_654@mail.ru>
2025-05-05 12:02:07 +0300

    добавлен redirect при логировании

--- frontend/src/pages/Login/LoginPage.tsx
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -1,6 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 import { LoginForm } from "../../components/loginForm/loginForm"
+import { useAppSelector } from "../../redux/hooks"
+import { useEffect } from 'react';
 
 export const LoginPage = () =>{
+    const navigate = useNavigate()
+    const isAuth = useAppSelector(state=>state.auth.isAuthenticated)
+    useEffect(()=>{
+        if(isAuth) navigate("/")
+    }, [isAuth, navigate])
     return(
     <div className="flex ai-center jc-center h-100 w-100">
         <div className="w-50"> 
