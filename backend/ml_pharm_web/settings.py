c57e1d7100fd87413d0ce696fa1eac0bfed2484c
Philipekum <filipp.tsekhansky@yandex.ru>
2025-05-05 18:38:06 +0300

    .env reading fix

--- backend/ml_pharm_web/settings.py
+++ backend/ml_pharm_web/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
+load_dotenv(override=True)
 
 from .logging import LOGGING
 
