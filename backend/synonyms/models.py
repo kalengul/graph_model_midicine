0d94a8e18fe9be9027b83526dabda1c93291460c
Philipekum <filipp.tsekhansky@yandex.ru>
2025-05-12 15:17:59 +0300

    added modified stubs

--- backend/synonyms/models.py
+++ backend/synonyms/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class SynonymGroup(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название группы")
+    is_completed = models.BooleanField(default=False, verbose_name="Завершена")
+
+    def __str__(self):
+        return self.name
+    
+
+class Synonym(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Слово")
+    group = models.ForeignKey(SynonymGroup, on_delete=models.CASCADE, related_name='synonyms')
+    is_changed = models.BooleanField(default=False, verbose_name="Удалено")
+
+    def __str__(self):
+        return self.name
+    
