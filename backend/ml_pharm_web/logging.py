e7eddee7626b20afccc3851eacdddce815f16706
Philipekum <filipp.tsekhansky@yandex.ru>
2025-05-26 11:56:44 +0300

    logger without files for debug

--- backend/ml_pharm_web/logging.py
+++ backend/ml_pharm_web/logging.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+from django.conf import settings
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -17,49 +19,51 @@ LOGGING = {
             'class': 'logging.StreamHandler',
             'formatter': 'detailed'
         },
-        'medscape_file': {
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs', 'medscape.log'),
-            'formatter': 'detailed',
-            'encoding': 'utf-8'
-        },
-        'fortran_file': {
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs', 'fortran.log'),
-            'formatter': 'detailed',
-            'encoding': 'utf-8'
-        },
-        'drugs_file': {
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs', 'drugs.log'),
-            'formatter': 'detailed',
-            'encoding': 'utf-8'
-        },
-        'apilog_file': {
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs', 'apilog.log'),
-            'formatter': 'detailed',
-            'encoding': 'utf-8'
-        },
+        **({} if settings.DEBUG else { 
+            'medscape_file': {
+                'class': 'logging.FileHandler',
+                'filename': os.path.join(BASE_DIR, 'logs', 'medscape.log'),
+                'formatter': 'detailed',
+                'encoding': 'utf-8'
+            },
+            'fortran_file': {
+                'class': 'logging.FileHandler',
+                'filename': os.path.join(BASE_DIR, 'logs', 'fortran.log'),
+                'formatter': 'detailed',
+                'encoding': 'utf-8'
+            },
+            'drugs_file': {
+                'class': 'logging.FileHandler',
+                'filename': os.path.join(BASE_DIR, 'logs', 'drugs.log'),
+                'formatter': 'detailed',
+                'encoding': 'utf-8'
+            },
+            'apilog_file': {
+                'class': 'logging.FileHandler',
+                'filename': os.path.join(BASE_DIR, 'logs', 'apilog.log'),
+                'formatter': 'detailed',
+                'encoding': 'utf-8'
+            },
+        })
     },
     'loggers': {
         'medscape': {
-            'handlers': ['medscape_file', 'console'],
+            'handlers': ['console'] if settings.DEBUG else ['medscape_file', 'console'],
             'level': 'DEBUG',
             'propagate': False
         },
         'fortran': {
-            'handlers': ['fortran_file', 'console'],
+            'handlers': ['console'] if settings.DEBUG else ['fortran_file', 'console'],
             'level': 'DEBUG',
             'propagate': False
         },
         'drugs': {
-            'handlers': ['drugs_file', 'console'],
+            'handlers': ['console'] if settings.DEBUG else ['drugs_file', 'console'],
             'level': 'DEBUG',
             'propagate': False
         },
         'apilog': {
-            'handlers': ['apilog_file', 'console'],
+            'handlers': ['console'] if settings.DEBUG else ['apilog_file', 'console'],
             'level': 'INFO',
             'propagate': False
         },
