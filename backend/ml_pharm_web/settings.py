643df9a0c9d738c689e05044f7af76ad4d212d99
Philipekum <filipp.tsekhansky@yandex.ru>
2025-09-17 13:42:49 +0300

    added max memory size

--- backend/ml_pharm_web/settings.py
+++ backend/ml_pharm_web/settings.py
@@ -3,6 +3,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100 MB
+FILE_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100 MB
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(override=True)
