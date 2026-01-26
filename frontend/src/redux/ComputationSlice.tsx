0575f2c30a44ac42dd58a56cb3b7600cf642fa84
PolinaWillow <polianna_654@mail.ru>
2026-01-26 15:07:25 +0300

    сортировка побочек по убыванию ранга

--- frontend/src/redux/ComputationSlice.tsx
+++ frontend/src/redux/ComputationSlice.tsx
@@ -387,6 +387,14 @@ const ComputationSlice = createSlice({
             state.resultBayes = action.payload.data
             state.isLoadBayes = true
             state.fetchBayesStatus = true
+
+            //Сортируем результаты по убыванию ранга попбочки
+            state.resultBayes.side_effects = state.resultBayes.side_effects.map(item => (
+              {
+                сompatibility: item.сompatibility,
+                effects: item.effects.sort((a, b) => b.rank - a.rank)
+              }
+            ))
           }
           else if ( action.payload.status === "err") {
             state.isresultBayes = false
