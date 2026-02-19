f415c92465df5e6ab8f788e934572e36621f17b4
Emhyr713 <kilmiskinn@gmail.com>
2026-02-19 12:17:35 +0300

    Исправлены пути до dict_synonym_contraindications

--- backend/pHistory2se/model_loader.py
+++ backend/pHistory2se/model_loader.py
@@ -1,15 +1,18 @@
 # backend/pHistory2se/utils/model_loader.py
-
 import os
 import json
+from pathlib import Path
+from django.conf import settings
 
-# from sentence_transformers import SentenceTransformer
 from pHistory2se.utils.SemanticEmbeddingProcessor import SemanticEmbeddingProcessor
 
 # Пути (можно вынести в settings.py позже)
 EMB_MODEL_PATH = 'data\\sentence_transformer_models\\all-MiniLM-L6-v2'
 # EMB_MODEL_PATH = 'all-MiniLM-L6-v2'
-SYNONYM_DICT_PATH = "data\\dictionaries\\dict_synonym_contraindications.json"
+SYNONYM_DICT_FILE = "dict_synonym_contraindications.json"
+
+DIR_PATH = Path(settings.SYNONYM_PATH)
+SYNONYM_DICT_PATH = DIR_PATH / SYNONYM_DICT_FILE
 
 # Глобальная переменная — будет инициализирована один раз
 _model = None
@@ -19,7 +22,6 @@ def get_embedding_processor():
     global _model
     if _model is None:
         print("Загрузка процессора эмбеддингов...")
-        # _model = SentenceTransformer(EMB_MODEL_PATH)
         _processor = SemanticEmbeddingProcessor(EMB_MODEL_PATH)
         print("SemanticEmbeddingProcessor загружен.")
     return _processor
