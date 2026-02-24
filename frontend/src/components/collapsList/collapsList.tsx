a43555c6d76a3ffc5ec2f4632512363445de8df1
Emhyr713 <kilmiskinn@gmail.com>
2026-02-24 22:26:27 +0300

    Добавлены разделители между побочек

--- frontend/src/components/collapsList/collapsList.tsx
+++ frontend/src/components/collapsList/collapsList.tsx
@@ -17,13 +17,16 @@ const RenderItem = (type: ICollapsListProps['type'], item: any, index: number, v
         case "riscs": {
             const riscItem = item as ISE;
             return(
-                <div className='flex jc-sb w-100 ps-3 pe-3' key={index}>
+                <>
+                <div className={flex jc-sb w-100 ps-3 pe-3 ${index===0 && "mt-3"}} key={index}>
                     <div>
                         <span className='me-3'>{index+1}.</span> 
                         <span>{riscItem.se_name}</span>
                     </div>
                     {visibleRisks && <span>{riscItem.rank}</span>}
                 </div>
+                <hr/>
+                </>
             )
         }
         case "compare-riscs":{
