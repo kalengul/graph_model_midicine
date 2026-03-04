2e66eec848c2cc21e5d4887c75f183968add5f30
PolinaWillow <polianna_654@mail.ru>
2026-03-04 18:10:55 +0300

    Исправлена опечатка

--- frontend/src/pages/ComputationFortran/ComputationFortran.tsx
+++ frontend/src/pages/ComputationFortran/ComputationFortran.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react"
+import {useEffect, useState } from "react"
 
 import { ComputationForm } from "../../components/computationForm/ComputationForm";
 import { Nav } from '../../components/nav/nav';
